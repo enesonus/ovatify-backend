06a7e0c850c254870a129cc2b3c97f2fb67e5d49
Bilal Dertli <127341202+bilalbdertli@users.noreply.github.com>
2023-11-07 11:58:09 +0300

    OVTF-101: Create User API implementation
    added deleteUser endpoint
    bug fixes on createUser and getall endpoints
    added django cors to project to handle errors,
    
    bypass token feature
    
    Performed User CRUD operations
    
    OVTF-101: Create User API implementation Bug Fixes

--- OVTF_Backend/settings.py
+++ OVTF_Backend/settings.py
@@ -33,12 +33,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+CORS_ALLOW_ALL_ORIGINS = True  # CORS
 
 # Application definition
 
 INSTALLED_APPS = [
     "songs",
     "users",
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -48,6 +50,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",  # CORS
+    "django.middleware.common.CommonMiddleware",  # CORS
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
