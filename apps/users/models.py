f01d0d9de30d6feef60c3e797382e3056476adab
enesonus <enes.onus@getir.com>
2023-11-11 20:06:37 +0300

    OVTF-122-dev: Change UUID field of user to Char field

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,5 +1,3 @@
-import uuid
-
 from django.db import models
 from django.utils import timezone
 
@@ -8,30 +6,29 @@ from apps.songs.models import Song
 
 # Enum Choices
 class TempoChoices(models.TextChoices):
-    SLOW = 'S', ('Slow')
-    MEDIUM = 'M', ('Medium')
-    FAST = 'F', ('Fast')
+    SLOW = "S", ("Slow")
+    MEDIUM = "M", ("Medium")
+    FAST = "F", ("Fast")
 
 
 class MoodChoices(models.TextChoices):
-    HAPPY = 'H', ('Happy')
-    SAD = 'SA', ('Sad')
-    EXCITED = 'E', ('Excited')
-    RELAXED = 'R', ('Relaxed')
+    HAPPY = "H", ("Happy")
+    SAD = "SA", ("Sad")
+    EXCITED = "E", ("Excited")
+    RELAXED = "R", ("Relaxed")
 
 
 class RecommendedEnvironmentChoices(models.TextChoices):
-    INDOOR = 'I', ('Indoor')
-    OUTDOOR = 'O', ('Outdoor')
-    STUDIO = 'S', ('Studio')
-    LIVE = 'L', ('Live')
+    INDOOR = "I", ("Indoor")
+    OUTDOOR = "O", ("Outdoor")
+    STUDIO = "S", ("Studio")
+    LIVE = "L", ("Live")
 
 
 # Models
 class User(models.Model):
-    firebase_uid = models.UUIDField(primary_key=True,
-                                    default=uuid.uuid4,
-                                    editable=False)
+    firebase_uid = models.CharField(max_length=200, primary_key=True)
+
     username = models.CharField(max_length=150)
     email = models.EmailField(unique=True)
     date_joined = models.DateTimeField(default=timezone.now)
@@ -45,11 +42,11 @@ class UserSongRating(models.Model):
     rating_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     song = models.ForeignKey(Song, on_delete=models.CASCADE)
-    rating = models.DecimalField(max_digits=3, decimal_places=2)
+    rating = models.DecimalField(max_digits=4, decimal_places=2)
     date_rated = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return f"{self.user.__str__} - {self.song.__str__}"
+        return f"{self.user.__str__()} - {self.song.__str__()}"
 
 
 class UserPreferences(models.Model):
@@ -59,7 +56,7 @@ class UserPreferences(models.Model):
     data_sharing_consent = models.BooleanField()
 
     def __str__(self):
-        return f"Preferences of {self.user.__str__}"
+        return f"Preferences of {self.user.__str__()}"
 
 
 class Instrument(models.Model):
@@ -73,10 +70,8 @@ class Instrument(models.Model):
 
 # Many-to-Many Relationship Tables
 class Friend(models.Model):
-    user = models.ForeignKey(User, related_name='user',
-                             on_delete=models.CASCADE)
-    friend = models.ForeignKey(User, related_name='friend',
-                               on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name="user", on_delete=models.CASCADE)
+    friend = models.ForeignKey(User, related_name="friend", on_delete=models.CASCADE)
 
     class Meta:
-        unique_together = (('user', 'friend'),)
+        unique_together = (("user", "friend"),)
