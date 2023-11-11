48cd5bc7e361cd38632345eaa9ba7abb09bda358
berkay can <cberkay@sabanciuniv.edu>
2023-11-11 17:56:51 +0300

    OVTF-111: User Preferences API endpoint

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,5 +4,6 @@ from apps.users import views
 
 urlpatterns = [
     path('get-all/', view=views.get_all_users, name='get-all-users'),
-    path('return-post-body/', view=views.return_post_body, name='return-post-body')
+    path('return-post-body/', view=views.return_post_body, name='return-post-body'),
+    path('user-preferences/',view = views.user_preferences_create, name='user_preferences_create')
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 from OVTF_Backend.firebase_auth import token_required
 from apps.users.models import User
-
+from apps.users.models import UserPreferences
 
 # Create endpoints
 
@@ -33,3 +33,25 @@ def return_post_body(request):
         "data": data
     }
     return JsonResponse(context, status=200)
+
+@csrf_exempt
+@token_required
+def user_preferences_create(request):
+    try:
+        data = json.loads(request.body.decode('utf-8'))
+
+        user, created = User.objects.get_or_create(username=data.get('user'))
+
+        user_preferences, preferences_created = UserPreferences.objects.get_or_create(user=user)
+
+        user_preferences.data_processing_consent = data.get('data_processing_consent', True)
+        user_preferences.data_sharing_consent = data.get('data_sharing_consent', True)
+        user_preferences.save()
+
+        if preferences_created:
+            return JsonResponse({'message': 'UserPreferences created successfully.'}, status=201)
+        else:
+            return JsonResponse({'message': 'UserPreferences updated successfully.'}, status=200)
+
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=400)
