09c7a6a436011bb96ed05e86abe6373819f1d38e
enesonus <enes.onus@getir.com>
2023-11-13 12:57:12 +0300

    fix

--- apps/songs/urls.py
+++ apps/songs/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
  path('get-all/', view=views.get_all_songs, name='get-all-songs'),
  path('api/add', views.add_song, name='add-song'),
  path('api/search', views.search_songs, name='search-songs'),
- path('api/get', views.get_songs, name='get-songs'),
- path('api/fetch', views.get_song, name='get-song'), 
 ]
