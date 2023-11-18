977fffe2c2f90b4e5bf23f12d7e0aad76b824f92
enesonus <enes.onus@getir.com>
2023-11-18 20:38:41 +0300

    OVTF-157-dev: DB Migrations

--- apps/songs/models.py
+++ apps/songs/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from OVTF_Backend.models import CoreModel
+
+import uuid
 
 # Enum Choices
 class Tempo(models.TextChoices):
@@ -15,7 +18,7 @@ class Mood(models.TextChoices):
     RELAXED = 'R', ('Relaxed')
 
 
-class RecommendedEnvironment(models.TextChoices):
+class RecordedEnvironment(models.TextChoices):
     INDOOR = 'I', ('Indoor')
     OUTDOOR = 'O', ('Outdoor')
     STUDIO = 'S', ('Studio')
@@ -23,62 +26,65 @@ class RecommendedEnvironment(models.TextChoices):
 
 
 # Models
-class Genre(models.Model):
-    genre_id = models.AutoField(primary_key=True)
-    genre_name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return str(self.genre_name)
-
-
-class Song(models.Model):
-    song_id = models.AutoField(primary_key=True)
-    track_name = models.CharField(max_length=200)
-    release_year = models.PositiveIntegerField()
-    length = models.DurationField()
-    tempo = models.CharField(max_length=1, choices=Tempo.choices)
-    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
-    mood = models.CharField(max_length=2, choices=Mood.choices)
-    recommended_environment = models.CharField(
-                              max_length=1,
-                              choices=RecommendedEnvironment.choices)
-    duration = models.PositiveIntegerField()
-    replay_count = models.PositiveIntegerField(default=0)
-    version = models.CharField(max_length=50)
-
-    def __str__(self):
-        return str(self.track_name)
-
-
-class Artist(models.Model):
-    artist_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200)
-    bio = models.TextField()
+class Genre(CoreModel):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    img_url = models.URLField(max_length=300, blank=True)
 
     def __str__(self):
         return str(self.name)
 
 
-class Album(models.Model):
-    album_id = models.AutoField(primary_key=True)
+class Song(CoreModel):
+    id = models.CharField(default=uuid.uuid4,
+                          primary_key=True, max_length=200)
+    name = models.CharField(max_length=200)
+    release_year = models.PositiveIntegerField()
+    duration = models.DurationField()
+    tempo = models.CharField(max_length=1, choices=Tempo.choices)
+    mood = models.CharField(max_length=2, choices=Mood.choices)
+    recorded_environment = models.CharField(
+                              max_length=1,
+                              choices=RecordedEnvironment.choices)
+    replay_count = models.PositiveIntegerField(default=0)
+    version = models.CharField(max_length=50, blank=True)
+    img_url = models.URLField(max_length=300, blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Artist(CoreModel):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    bio = models.TextField()
+    img_url = models.URLField(max_length=300, blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Album(CoreModel):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
     release_date = models.DateField()
+    img_url = models.URLField(max_length=300, blank=True)
 
     def __str__(self):
         return str(self.title)
 
 
-class Instrument(models.Model):
-    instrument_id = models.AutoField(primary_key=True)
-    instrument_type = models.CharField(max_length=100)
-    instrument_name = models.CharField(max_length=100)
+class Instrument(CoreModel):
+    id = models.AutoField(primary_key=True)
+    type = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
-        return str(self.instrument_name)
+        return str(self.name)
 
 
 # Many-to-Many Relationship Tables
-class SongArtist(models.Model):
+class ArtistSong(CoreModel):
     song = models.ForeignKey(Song, on_delete=models.CASCADE)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
 
@@ -86,7 +92,7 @@ class SongArtist(models.Model):
         unique_together = (('song', 'artist'),)
 
 
-class AlbumSong(models.Model):
+class AlbumSong(CoreModel):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     song = models.ForeignKey(Song, on_delete=models.CASCADE)
 
@@ -94,7 +100,7 @@ class AlbumSong(models.Model):
         unique_together = (('album', 'song'),)
 
 
-class GenreSong(models.Model):
+class GenreSong(CoreModel):
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     song = models.ForeignKey(Song, on_delete=models.CASCADE)
 
@@ -102,7 +108,7 @@ class GenreSong(models.Model):
         unique_together = (('genre', 'song'),)
 
 
-class InstrumentSong(models.Model):
+class InstrumentSong(CoreModel):
     instrument = models.ForeignKey(Instrument, on_delete=models.CASCADE)
     song = models.ForeignKey(Song, on_delete=models.CASCADE)
 
