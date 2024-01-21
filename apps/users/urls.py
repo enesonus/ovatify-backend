eed5a0284d7590e360fc8c1704509e578c957214
Bilal Dertli <127341202+bilalbdertli@users.noreply.github.com>
2024-01-21 11:53:21 +0300

    Testing Stuff

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('login/', view=views.login, name='login'),
     path('delete-user/', view=views.delete_user, name='delete-user'),
     path('update-user/', view=views.update_user, name='update-user'),
-    path('user-preferences/', view=views.user_preferences_create, name='user_preferences_create'),
+    path('user-preferences/', view=views.user_preferences_create, name='user-preferences-create'),
     path('user-songs/', view=views.user_songs_view, name='user-songs-view'),
     path('hello-github/', view=views.hello_github, name='hello-github'),
     path('add-song-rating/', view=views.add_song_rating, name='add-song-rating'),
